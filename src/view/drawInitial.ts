4c6224324c974db675c7e0d579b8744a246de768
Mina <minap2001@gmail.com>
2023-08-15 01:34:50 +0200

    initDraw started

--- src/view/drawInitial.ts
+++ src/view/drawInitial.ts
@@ -2,12 +2,14 @@ export function drawInit(host: HTMLElement) {
     const container:HTMLElement=drawDiv("main-container",host);
     const left:HTMLElement=drawDiv("left-container",container);
     const right:HTMLElement=drawDiv("right-container",container);
+    drawNumberPickers(left);
+    initialDrawExperts(left);
   }
 
-  function drawNumberPickers(host: HTMLElement){
-    const container = document.createElement("div");
-    container.classList.add("numbers-container");
-    host.appendChild(container);
+  function drawNumberPickers(host: HTMLElement):HTMLElement{
+    const container:HTMLElement=drawDiv("numbers-container",host);
+    createNumberInput("Br. alternativa",container,"alternativa-num-input",2,10);
+    createNumberInput("Br. kriterijuma",container,"kriterijum-num-input",1,5);
     return container;
   }
 
@@ -17,3 +19,40 @@ export function drawInit(host: HTMLElement) {
     host.appendChild(container);
     return container;
   }
+
+  function createNumberInput(labelText: string,host:HTMLElement,className:string,minValue:number,maxValue:number): HTMLInputElement {
+    const label = document.createElement('label');
+    label.textContent = labelText;
+
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.min = minValue.toString();
+    input.max = maxValue.toString();
+    input.value=minValue.toString();;
+    input.classList.add(className);
+
+    host.appendChild(label);
+    host.appendChild(input);
+
+    return input;
+}
+
+function drawExpert(host: HTMLElement){
+
+}
+
+function initialDrawExperts(host: HTMLElement){
+
+}
+
+function initDrawWeights(host: HTMLElement){
+
+}
+
+export function initDrawMatrix(host: HTMLElement,altNum:number,critNum:number){
+
+}
+
+export function initDrawPie(host: HTMLElement){
+
+}
