d8cb10259e5fdf274b3e56b2fa112a105b56284f
Mina <minap2001@gmail.com>
2023-08-19 19:42:56 +0200

    matrix showing

--- src/view/drawInitial.ts
+++ src/view/drawInitial.ts
@@ -1,6 +1,6 @@
 import { fromEvent } from "rxjs";
 import { combineHandlers, handleNumberChange } from "../logic/eventHandlers";
-import { getCriteriaNumber } from "../globals";
+import { getAlternativeNumber, getCriteriaNumber } from "../globals";
 
 export function drawInit(host: HTMLElement) {
     const container:HTMLElement=drawDiv("main-container",host);
@@ -9,6 +9,7 @@ export function drawInit(host: HTMLElement) {
     drawNumberPickers(left);
     initialDrawExperts(left);
     initDrawWeights(left);
+    initDrawRight(right);
   }
 
   function drawNumberPickers(host: HTMLElement):HTMLElement{
@@ -18,7 +19,7 @@ export function drawInit(host: HTMLElement) {
     return container;
   }
 
-  function drawDiv(className:string,host:HTMLElement):HTMLElement{
+ export function drawDiv(className:string,host:HTMLElement):HTMLElement{
     const container = document.createElement("div");
     container.classList.add(className);
     host.appendChild(container);
@@ -82,7 +83,15 @@ function initialDrawExperts(host: HTMLElement){
 }
 
 function initDrawWeights(host: HTMLElement){
-  const container:HTMLElement=drawDiv("weights-container",host);
+  const containerExisting = document.querySelector('.weights-container');
+  let container:HTMLElement;
+  if(containerExisting===null){
+    container=drawDiv("weights-container",host);
+  }else{
+    container=containerExisting as HTMLElement;
+    container.innerHTML="";
+  }
+ 
   const label:HTMLElement=document.createElement("label");
   label.textContent="Weights:";
   container.appendChild(label);
@@ -91,23 +100,127 @@ function initDrawWeights(host: HTMLElement){
   }
 }
 
-function drawWeightCalculated(host: HTMLElement,critNum:number){
+function drawWeightCalculated(host: HTMLElement,critNum:number,weight:number=0){
   const container:HTMLElement=drawDiv("weight-container",host);
   const label:HTMLElement=document.createElement("label");
   label.textContent="K"+critNum;
   container.appendChild(label);
   const input:HTMLInputElement=document.createElement("input");
   input.type="number";
-  input.value="0";
+  input.value=weight.toString();
   input.readOnly=true;
   input.classList.add("weight-input");
   container.appendChild(input);
 }
 
-export function initDrawMatrix(host: HTMLElement,altNum:number,critNum:number){
-
+export function initDrawRight(host: HTMLElement){
+  host.innerHTML="";
+  drawNameInputs(host);
+  drawMatrix(host);
 }
 
-export function initDrawPie(host: HTMLElement){
+export function drawMatrix(host: HTMLElement){
+  const containerExisting = document.querySelector('.matrix-container');
+  let container:HTMLElement;
+
+  if(containerExisting===null){
+    container=drawDiv("matrix-container",host);
+  }else{
+    container=containerExisting as HTMLElement;
+    container.innerHTML="";
+  }
+  const label:HTMLElement=document.createElement("label");
+  label.textContent="Matrix:";
+  container.appendChild(label);
+
+  const containerCrits:HTMLElement=drawDiv("matrix-row-container",container);
+  const labelRow:HTMLElement=document.createElement("label");
+  labelRow.textContent=".";
+  containerCrits.appendChild(labelRow);
+  for(let i=0;i<getCriteriaNumber();i++){
+    const label:HTMLElement=document.createElement("label");
+    label.textContent="C"+(i+1);
+    containerCrits.appendChild(label);
+  }
+ 
+  for(let i=0;i<getAlternativeNumber();i++){
+    const containerRow:HTMLElement=drawDiv("matrix-row-container",container);
+    const label:HTMLElement=document.createElement("label");
+    label.textContent="A"+(i+1);
+    containerRow.appendChild(label);
+    for(let j=0;j<getCriteriaNumber();j++){
+      drawMatrixInput(containerRow,i+1,j+1);
+    }
+  }
+
+
+  
+}
+
+function drawMatrixInput(container: HTMLElement, arg1: number, arg2: number) {
+  const input:HTMLInputElement=document.createElement("input");
+  input.type="number";
+  input.min="1";
+  input.max="10";
+  input.value="5";
+  input.classList.add("matrix-input"+arg1.toString()+arg2.toString());
+  container.appendChild(input);
+}
+
+
+
+function drawNameInputs(container: HTMLElement) {
+  const containerExisting = document.querySelector('.name-inputs-container');
+  let containerName:HTMLElement;
+  if(containerExisting===null){
+    containerName=drawDiv("name-inputs-container",container);
+  }
+  else{
+    containerName=containerExisting as HTMLElement;
+    containerName.innerHTML="";
+  }
+
+  const altNamesContainer:HTMLElement=drawDiv("alt-names-container",containerName)
+  const label:HTMLElement=document.createElement("label");
+  label.textContent="Names of alternatives:";
+  altNamesContainer.appendChild(label);
+  for(let i=0;i<getAlternativeNumber();i++){
+    const input:HTMLInputElement=document.createElement("input");
+    input.type="text";
+    input.value="A"+(i+1);
+    input.classList.add("alt-name-input"+i);
+    altNamesContainer.appendChild(input);
+  }
+
+  const crtNamesContainer:HTMLElement=drawDiv("crt-names-container",containerName)
+  const labelcrt:HTMLElement=document.createElement("label");
+  labelcrt.textContent="Names of criterias:";
+  crtNamesContainer.appendChild(labelcrt);
+  for(let i=0;i<getCriteriaNumber();i++){
+    const input:HTMLInputElement=document.createElement("input");
+    input.type="text";
+    input.value="C"+(i+1);
+    input.classList.add("crt-name-input"+i);
+    crtNamesContainer.appendChild(input);
+  }
+}
+
+
+export function initDrawPie(host: HTMLElement, values:number[],labels:string[]){
+  const containerExisting = document.querySelector('.pie-container');
+  let container:HTMLElement;
+
+  if(containerExisting===null){
+    container=drawDiv("pie-container",host);
+  }else{
+    container=containerExisting as HTMLElement;
+    container.innerHTML="";
+  }
+
+  const label:HTMLElement=document.createElement("label");
+  label.textContent="Result:";
+  container.appendChild(label);
+
+  
 
 }
